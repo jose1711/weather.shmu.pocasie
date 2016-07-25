0d7e52ed90cdec97d814273b2ddbfbe22b910691
jose1711 <jose1711@gmail.com>
2016-07-25 21:46:49 +0200

    Apply PEP8

--- default.py
+++ default.py
@@ -15,67 +15,82 @@
 # *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
 # *  http://www.gnu.org/copyleft/gpl.html
 
-import os, sys, socket, urllib2, util, bs4
 from demjson import demjson
-import xbmc, xbmcgui, xbmcaddon
-import re, datetime
-from stats import *
-import math
 from PIL import Image
+from stats import *
+import bs4
 import cStringIO
+import datetime
+import math
+import os
+import re
+import socket
+import sys
+import urllib2
+import util
+import xbmc
+import xbmcaddon
+import xbmcgui
 
 
 # Nacitanie informacii o doplnku
-__addon__      = xbmcaddon.Addon()
-__addonname__  = __addon__.getAddonInfo('name')
-__addonid__    = __addon__.getAddonInfo('id')
-__cwd__        = __addon__.getAddonInfo('path').decode("utf-8")
-__language__   = __addon__.getLocalizedString
-PROFILE      = xbmc.translatePath(__addon__.getAddonInfo('profile')).decode('utf-8')
+__addon__ = xbmcaddon.Addon()
+__addonname__ = __addon__.getAddonInfo('name')
+__addonid__ = __addon__.getAddonInfo('id')
+__cwd__ = __addon__.getAddonInfo('path').decode("utf-8")
+__language__ = __addon__.getLocalizedString
+PROFILE = xbmc.translatePath(__addon__.getAddonInfo('profile')).decode('utf-8')
+
 
 def log(msg):
-    xbmc.log(("### [%s] - %s" % (__addonname__.decode('utf-8'), msg.decode('utf-8'))).encode('utf-8'), level=xbmc.LOGDEBUG)
+    xbmc.log(("### [%s] - %s" % (__addonname__.decode('utf-8'),
+                                 msg.decode('utf-8'))).encode('utf-8'), level=xbmc.LOGDEBUG)
 
 # Vseobecne nastavenia
 _UserAgent_ = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3'
-WEATHER_WINDOW   = xbmcgui.Window(12600)
+WEATHER_WINDOW = xbmcgui.Window(12600)
 socket.setdefaulttimeout(10)
 
 # Vypisovanie nazvov dnov v Slovencine
-skdays = ['Nedeľa', 'Pondelok', 'Utorok', 'Streda', 'Štvrtok', 'Piatok', 'Sobota', 'Nedeľa', 'Pondelok', 'Utorok', 'Streda', 'Štvrtok', 'Piatok', 'Sobota', 'Nedeľa']
+skdays = ['Nedeľa', 'Pondelok', 'Utorok', 'Streda', 'Štvrtok', 'Piatok', 'Sobota',
+          'Nedeľa', 'Pondelok', 'Utorok', 'Streda', 'Štvrtok', 'Piatok', 'Sobota', 'Nedeľa']
 den = datetime.datetime.now()
 den = int(den.strftime("%w"))
-log('den: %s' %den)
+log('den: %s' % den)
 
 # Nastavenie hodnot o pocasi
+
+
 def set_property(name, value):
     WEATHER_WINDOW.setProperty(name, value)
 
-en2icon={
-  'rain': 'dest',
-  'clear': 'jasno',
-  'clouds': 'oblacno',
-  'thunderstorm': 'bourky',
+en2icon = {
+    'rain': 'dest',
+    'clear': 'jasno',
+    'clouds': 'oblacno',
+    'thunderstorm': 'bourky',
 }
 
-WEATHER_CODES={
+WEATHER_CODES = {
     '1': '32',
-    '2': '34', # mala oblacnost
+    '2': '34',  # mala oblacnost
     '3': '26',
-    '4': '40', # dazd
+    '4': '40',  # dazd
     '5': '39',
     '6': '17',
     '7': '18',
     '8': '6',
     '9': '20',
-    '10': '26', # oblacno
+    '10': '26',  # oblacno
     '11': '39',
-    '12': '30', # mala oblacnost
+    '12': '30',  # mala oblacnost
     '13': '42',
-    '16': '26', # zamracene
-    }
-    
+    '16': '26',  # zamracene
+}
+
 # Nacita udaje o pocasi zo servera
+
+
 def parse_data():
     # Stiahnutie udajov
     mesto = __addon__.getSetting('mesto')
@@ -83,95 +98,99 @@ def parse_data():
     key = __addon__.getSetting('key')
 
     if not key:
-        xbmcgui.Dialog().ok('Chyba','Zadajte v nastaveniach kľúč k OpenWeather API!')
+        xbmcgui.Dialog().ok('Chyba', 'Zadajte v nastaveniach kľúč k OpenWeather API!')
         return True
 
-    data = {'mesto':mesto,'page':'1','id':'meteo_predpoved_sk'}
+    data = {'mesto': mesto, 'page': '1', 'id': 'meteo_predpoved_sk'}
     url = 'http://www.shmu.sk/sk/?#tab'
-    page = util.post(url,data)
-    soup = bs4.BeautifulSoup(page,"html5lib")
+    page = util.post(url, data)
+    soup = bs4.BeautifulSoup(page, "html5lib")
     cnt = 0
-    for x in soup.select('.w600')[0].tbody.findAll('td','center'):
+    for x in soup.select('.w600')[0].tbody.findAll('td', 'center'):
         if x.has_attr('style'):
             if 'white-space' in x['style']:
-                set_property('Daily.%s.LongDay' % cnt  , skdays[den+cnt])
-                set_property('Daily.%s.ShortDay' % cnt  , skdays[den+cnt])
-                night,day=x.get_text(separator='|').split('|')
+                set_property('Daily.%s.LongDay' % cnt, skdays[den + cnt])
+                set_property('Daily.%s.ShortDay' % cnt, skdays[den + cnt])
+                night, day = x.get_text(separator='|').split('|')
                 set_property('Daily.%s.HighTemperature' % cnt, day)
                 set_property('Daily.%s.LowTemperature' % cnt, night)
             elif 'background:#00660E' in x['style']:
-                imgname=x.img['src'].split('/')[-1]
+                imgname = x.img['src'].split('/')[-1]
                 set_property('Daily.%s.Outlook' % cnt, x.img['alt'])
-                image_name=x.img['src'].split('/')[-1]
-                set_property('Daily.%s.OutlookIcon' % cnt, WEATHER_CODES[image_name.replace('.gif','')]+'.png')
-	        cnt += 1
+                image_name = x.img['src'].split('/')[-1]
+                set_property('Daily.%s.OutlookIcon' % cnt, WEATHER_CODES[
+                             image_name.replace('.gif', '')] + '.png')
+                cnt += 1
 
     url = 'http://api.openweathermap.org/data/2.5/find?q=%s&type=like&mode=json&APPID=%s&units=metric' \
-            % (urllib2.quote(mesto), key)
+        % (urllib2.quote(mesto), key)
     req = urllib2.urlopen(url)
     response = req.read()
     req.close()
-    jsonresponse=demjson.decode(response)['list'][0]
+    jsonresponse = demjson.decode(response)['list'][0]
 
-    set_property('Current.Temperature'   , str(jsonresponse['main']['temp']))
-    set_property('Current.Wind'   , str(jsonresponse['wind']['speed']))
-    set_property('Current.FeelsLike'     , feelslike(round(float(jsonresponse['main']['temp'])),
-                                            int(round(float(jsonresponse['wind']['speed'])*3.6) + 0.5)))
-    set_property('Current.Humidity'   , str(jsonresponse['main']['humidity']))
-    set_property('Current.DewPoint'      , dewpoint(round(float(jsonresponse['main']['temp'])),
-                                            int(jsonresponse['main']['humidity'])))
-    set_property('Current.Pressure'   , str(jsonresponse['main']['pressure']))
-    set_property('Current.Condition'   , str(jsonresponse['weather'][0]['main']))
-    set_property('Current.OutlookIcon'   , xbmc.translatePath(os.path.join(__cwd__, 'resources/lib/icons', '%s.png' % en2icon[jsonresponse['weather'][0]['main'].lower()])))
+    set_property('Current.Temperature', str(jsonresponse['main']['temp']))
+    set_property('Current.Wind', str(jsonresponse['wind']['speed']))
+    set_property('Current.FeelsLike', feelslike(round(float(jsonresponse['main']['temp'])),
+                                                int(round(float(jsonresponse['wind']['speed']) * 3.6) + 0.5)))
+    set_property('Current.Humidity', str(jsonresponse['main']['humidity']))
+    set_property('Current.DewPoint', dewpoint(round(float(jsonresponse['main']['temp'])),
+                                              int(jsonresponse['main']['humidity'])))
+    set_property('Current.Pressure', str(jsonresponse['main']['pressure']))
+    set_property('Current.Condition', str(jsonresponse['weather'][0]['main']))
+    set_property('Current.OutlookIcon', xbmc.translatePath(os.path.join(
+        __cwd__, 'resources/lib/icons', '%s.png' % en2icon[jsonresponse['weather'][0]['main'].lower()])))
     meteogrampage = util.parse_html('http://www.shmu.sk/sk/?page=1&id=meteo_num_mgram')
-    cityid=meteogrampage.select('select#nwp_mesto')[0].find(text=mestometeogram).parent['value']
-    day,month,year,hour,text=re.split('[. ]',meteogrampage.select('select[class=w150] option')[-1].text)
-    meteogramdate='%s%s%s-%s00' % (year, month, day, hour)
-    query='http://www.shmu.sk/data/datanwp/v2/'+\
-          'meteogram/al-meteogram_%s-%s-nwp-.png' \
-          % ( cityid, meteogramdate)
+    cityid = meteogrampage.select('select#nwp_mesto')[0].find(text=mestometeogram).parent['value']
+    day, month, year, hour, text = re.split(
+        '[. ]', meteogrampage.select('select[class=w150] option')[-1].text)
+    meteogramdate = '%s%s%s-%s00' % (year, month, day, hour)
+    query = 'http://www.shmu.sk/data/datanwp/v2/' +\
+        'meteogram/al-meteogram_%s-%s-nwp-.png' \
+        % (cityid, meteogramdate)
     req = urllib2.Request(query)
     response = urllib2.urlopen(req, timeout=10)
-    meteogramimage=Image.open(cStringIO.StringIO(response.read()))
-    headerimage=meteogramimage.crop(box=(0,0,600,45))
+    meteogramimage = Image.open(cStringIO.StringIO(response.read()))
+    headerimage = meteogramimage.crop(box=(0, 0, 600, 45))
     response.close()
 
-    aladin_text=['Teplota, oblačnosť, zrážky','Tlak, rýchlosť a smer vetra']
+    aladin_text = ['Teplota, oblačnosť, zrážky', 'Tlak, rýchlosť a smer vetra']
 
     set_property('Map.IsFetched', '')
-    for i in range(0,2):
-        outfilename=os.path.join(PROFILE, 'aladin%s.png' % (i+1))
-        imgfile=open(outfilename,'wb')
+    for i in range(0, 2):
+        outfilename = os.path.join(PROFILE, 'aladin%s.png' % (i + 1))
+        imgfile = open(outfilename, 'wb')
         out = Image.new("RGBA", (756, 756), None)
-        currview=meteogramimage.crop(box=(0,45+430*i,600,430*(i+1)+45))
-        out.paste(headerimage, (75,75))
-        out.paste(currview, (75,75+45))
+        currview = meteogramimage.crop(box=(0, 45 + 430 * i, 600, 430 * (i + 1) + 45))
+        out.paste(headerimage, (75, 75))
+        out.paste(currview, (75, 75 + 45))
         out.save(imgfile)
-        #out.close()
+        # out.close()
         imgfile.close()
-        set_property('Map.%s.Area' % str(i+1), outfilename)
-        set_property('Map.%s.Layer' % str(i+1), outfilename)
-        set_property('Map.%s.Heading' % str(i+1), aladin_text[i])
+        set_property('Map.%s.Area' % str(i + 1), outfilename)
+        set_property('Map.%s.Layer' % str(i + 1), outfilename)
+        set_property('Map.%s.Heading' % str(i + 1), aladin_text[i])
     set_property('Map.IsFetched', 'true')
 
-    
-def clear(): # Vynulovanie hodnot pred stiahnutim novych
-    set_property('Current.Condition'     , 'N/A')
-    set_property('Current.Temperature'   , '0')
-    set_property('Current.Wind'          , '0')
-    #set_property('Current.WindDirection' , 'N/A')
-    set_property('Current.Humidity'      , '0')
-    set_property('Current.OutlookIcon'   , 'na.png')
-    set_property('Current.FanartCode'    , 'na')
-    for count in range (0, 4):
-        set_property('Daily.%i.Title'              % count, 'N/A')
-        set_property('Daily.%i.HighTemperature'    % count, '0')
-        set_property('Daily.%i.LowTempTemperature' % count, '0')
-        set_property('Daily.%i.Outlook'            % count, 'N/A')
-        set_property('Daily.%i.OutlookIcon'        % count, 'na.png')
-        set_property('Daily.%i.FanartCode'         % count, 'na')
 
-def feelslike( T=10, V=25 ): #Pomocna funkce pro vypocet pocitove teploty
+def clear():  # Vynulovanie hodnot pred stiahnutim novych
+    set_property('Current.Condition', 'N/A')
+    set_property('Current.Temperature', '0')
+    set_property('Current.Wind', '0')
+    #set_property('Current.WindDirection' , 'N/A')
+    set_property('Current.Humidity', '0')
+    set_property('Current.OutlookIcon', 'na.png')
+    set_property('Current.FanartCode', 'na')
+    for count in range(0, 4):
+        set_property('Daily.%i.Title' % count, 'N/A')
+        set_property('Daily.%i.HighTemperature' % count, '0')
+        set_property('Daily.%i.LowTempTemperature' % count, '0')
+        set_property('Daily.%i.Outlook' % count, 'N/A')
+        set_property('Daily.%i.OutlookIcon' % count, 'na.png')
+        set_property('Daily.%i.FanartCode' % count, 'na')
+
+
+def feelslike(T=10, V=25):  # Pomocna funkce pro vypocet pocitove teploty
     """ The formula to calculate the equivalent temperature related to the wind chill is:
         T(REF) = 13.12 + 0.6215 * T - 11.37 * V**0.16 + 0.3965 * T * V**0.16
         Or:
@@ -183,103 +202,109 @@ def feelslike( T=10, V=25 ): #Pomocna funkce pro vypocet pocitove teploty
         getFeelsLike( tCelsius, windspeed )
     """
     FeelsLike = T
-    #Wind speeds of 4 mph or less, the wind chill temperature is the same as the actual air temperature.
-    if round( ( V + .0 ) / 1.609344 ) > 4:
-        FeelsLike = ( 13.12 + ( 0.6215 * T ) - ( 11.37 * V**0.16 ) + ( 0.3965 * T * V**0.16 ) )
-    return str( round( FeelsLike ) )
+    # Wind speeds of 4 mph or less, the wind chill temperature is the same as
+    # the actual air temperature.
+    if round((V + .0) / 1.609344) > 4:
+        FeelsLike = (13.12 + (0.6215 * T) - (11.37 * V**0.16) + (0.3965 * T * V**0.16))
+    return str(round(FeelsLike))
 
-def dewpoint( Tc=0, RH=93, minRH=( 0, 0.075 )[ 0 ] ): #Pomocna funkce pro vypocet rosneho bodu
+
+def dewpoint(Tc=0, RH=93, minRH=(0, 0.075)[0]):  # Pomocna funkce pro vypocet rosneho bodu
     """ Dewpoint from relative humidity and temperature
         If you know the relative humidity and the air temperature,
         and want to calculate the dewpoint, the formulas are as follows.
-    
+
         getDewPoint( tCelsius, humidity )
     """
-    #First, if your air temperature is in degrees Fahrenheit, then you must convert it to degrees Celsius by using the Fahrenheit to Celsius formula.
+    # First, if your air temperature is in degrees Fahrenheit, then you must convert it to degrees Celsius by using the Fahrenheit to Celsius formula.
     # Tc = 5.0 / 9.0 * ( Tf - 32.0 )
-    #The next step is to obtain the saturation vapor pressure(Es) using this formula as before when air temperature is known.
-    Es = 6.11 * 10.0**( 7.5 * Tc / ( 237.7 + Tc ) )
-    #The next step is to use the saturation vapor pressure and the relative humidity to compute the actual vapor pressure(E) of the air. This can be done with the following formula.
-    #RH=relative humidity of air expressed as a percent. or except minimum(.075) humidity to abort error with math.log.
-    RH = RH or minRH #0.075
-    E = ( RH * Es ) / 100
-    #Note: math.log( ) means to take the natural log of the variable in the parentheses
-    #Now you are ready to use the following formula to obtain the dewpoint temperature.
+    # The next step is to obtain the saturation vapor pressure(Es) using this
+    # formula as before when air temperature is known.
+    Es = 6.11 * 10.0**(7.5 * Tc / (237.7 + Tc))
+    # The next step is to use the saturation vapor pressure and the relative humidity to compute the actual vapor pressure(E) of the air. This can be done with the following formula.
+    # RH=relative humidity of air expressed as a percent. or except
+    # minimum(.075) humidity to abort error with math.log.
+    RH = RH or minRH  # 0.075
+    E = (RH * Es) / 100
+    # Note: math.log( ) means to take the natural log of the variable in the parentheses
+    # Now you are ready to use the following formula to obtain the dewpoint temperature.
     try:
-        DewPoint = ( -430.22 + 237.7 * math.log( E ) ) / ( -math.log( E ) + 19.08 )
+        DewPoint = (-430.22 + 237.7 * math.log(E)) / (-math.log(E) + 19.08)
     except ValueError:
-        #math domain error, because RH = 0%
-        #return "N/A"
-        DewPoint = 0 #minRH
-    #Note: Due to the rounding of decimal places, your answer may be slightly different from the above answer, but it should be within two degrees.
-    return str( int( DewPoint ) )
+        # math domain error, because RH = 0%
+        # return "N/A"
+        DewPoint = 0  # minRH
+    # Note: Due to the rounding of decimal places, your answer may be slightly
+    # different from the above answer, but it should be within two degrees.
+    return str(int(DewPoint))
 
 
 def settings():
     # Vyparsuj a zobraz mesta
-    dialog      = xbmcgui.Dialog()
-    mestalist1=[
-'BANSKÁ BYSTRICA',
-'BARDEJOV',
-'BRATISLAVA',
-'BREZNO',
-'DOLNÝ KUBÍN',
-'DUNAJSKÁ STREDA',
-'HURBANOVO',
-'KOMÁRNO',
-'KOŠICE',
-'KRÁĽ. CHLMEC',
-'LEVICE',
-'LIPTOVSKÝ MIKULÁŠ',
-'LUČENEC',
-'MEDZILABORCE',
-'MICHALOVCE',
-'NITRA',
-'PEZINOK',
-'PIEŠŤANY',
-'POPRAD',
-'PREŠOV',
-'PRIEVIDZA',
-'RIMAVSKÁ SOBOTA',
-'ROŽŇAVA',
-'SENICA',
-'SKALICA',
-'ŠAHY',
-'TRENČÍN',
-'TRNAVA',
-'VEĽKÝ KRTÍŠ',
-'ŽILINA',
+    dialog = xbmcgui.Dialog()
+    mestalist1 = [
+        'BANSKÁ BYSTRICA',
+        'BARDEJOV',
+        'BRATISLAVA',
+        'BREZNO',
+        'DOLNÝ KUBÍN',
+        'DUNAJSKÁ STREDA',
+        'HURBANOVO',
+        'KOMÁRNO',
+        'KOŠICE',
+        'KRÁĽ. CHLMEC',
+        'LEVICE',
+        'LIPTOVSKÝ MIKULÁŠ',
+        'LUČENEC',
+        'MEDZILABORCE',
+        'MICHALOVCE',
+        'NITRA',
+        'PEZINOK',
+        'PIEŠŤANY',
+        'POPRAD',
+        'PREŠOV',
+        'PRIEVIDZA',
+        'RIMAVSKÁ SOBOTA',
+        'ROŽŇAVA',
+        'SENICA',
+        'SKALICA',
+        'ŠAHY',
+        'TRENČÍN',
+        'TRNAVA',
+        'VEĽKÝ KRTÍŠ',
+        'ŽILINA',
     ]
 
     log("Mesta zoznam: %s" % mestalist1)
-    mesto       = dialog.select('Vyberte mesto', mestalist1)
-    
+    mesto = dialog.select('Vyberte mesto', mestalist1)
+
     meteogrampage = util.parse_html('http://www.shmu.sk/sk/?page=1&id=meteo_num_mgram')
-    mestalist2=meteogrampage.select('select#nwp_mesto')[0].get_text(separator='|').split('|')
+    mestalist2 = meteogrampage.select('select#nwp_mesto')[0].get_text(separator='|').split('|')
     dialog2 = xbmcgui.Dialog()
-    mestometeogram=dialog.select('Vyberte mesto (meteogram)', mestalist2)
+    mestometeogram = dialog.select('Vyberte mesto (meteogram)', mestalist2)
 
     # Uloz nastavenia
     __addon__.setSetting('mesto', mestalist1[mesto])
     __addon__.setSetting('mestometeogram', mestalist2[mestometeogram])
-    
+
 # Hlavny program
 
 # Zobraz nastavenia
-if sys.argv[1].startswith('mesto'): 
+if sys.argv[1].startswith('mesto'):
     settings()
 
 # Vycisti data
-clear()   
-      
+clear()
+
 # Dopln aktualne data
-parse_data()    
+parse_data()
 
 # Vseobecne informacie
 set_property('Location1',           __addon__.getSetting('mesto'))
 set_property('Locations',           str(1))
 set_property('WeatherProvider',     __addonname__)
-set_property('WeatherProviderLogo', xbmc.translatePath(os.path.join(__cwd__, 'resources', 'banner.png')))
+set_property('WeatherProviderLogo', xbmc.translatePath(
+    os.path.join(__cwd__, 'resources', 'banner.png')))
 set_property('Forecast.IsFetched', 'true')
 set_property('Daily.IsFetched', 'true')
 set_property('Map.IsFetched', 'true')
